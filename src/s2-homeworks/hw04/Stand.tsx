c2ad64f8c082443379fd05af03732f34424a897d
«Elena <LenaRybinskova@gmail.com>
2023-08-14 00:13:11 +0300

    hw4 upd

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -81,7 +81,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                       /* onChange={(e) =>setChecked(e.currentTarget.checked)}*/
+                        onChange={(e) =>setChecked(e.currentTarget.checked)}
 
                     />
                 </div>
