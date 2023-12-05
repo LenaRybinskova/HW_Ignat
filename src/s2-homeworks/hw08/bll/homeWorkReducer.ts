19d6d78a3709a71c66dcd1bb4de2c47cf58a4d76
«Elena <LenaRybinskova@gmail.com>
2023-12-05 16:34:15 +0300

    hw8 без стилей, исправлен редьюсор

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,12 +8,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': {
             if (action.payload === 'up') {
-                return state.sort((a, b) => a.name.localeCompare(b.name))
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
             }
-            return state.sort((a, b) => b.name.localeCompare(a.name))
+            return [...state].sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
-            return state.filter(u => u.age > action.payload)
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
