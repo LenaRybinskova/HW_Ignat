90a904ab2d6f9e5c2186c58314026048a2047ae0
«Elena <LenaRybinskova@gmail.com>
2024-04-03 17:10:20 +0300

    hw11 upd

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,14 +15,15 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: Event, value: number[] | number) => {
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        if (typeof value == 'number') {
-            setValue1(value)
-        } else {
+        if (value instanceof Array) {
             setValue1(value[0])
             setValue2(value[1])
+        } else {
+            setValue1(value)
         }
+
     }
 
     return (
@@ -34,16 +35,21 @@ function HW11() {
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
-                            id={'hw11-double-slider'}
+                            id={'hw11-single-slider'}
+                            // сделать так чтоб value1 изменялось // пишет студент
                             onChange={change}
-                            value={value1}/>
+                            value={value1}
+                        />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
-                            id={'hw11-double-slider1'}
+                            id={'hw11-double-slider'}
+                            // сделать так чтоб value1/2 изменялось // пишет студент
                             onChange={change}
-                            value={[value1, value2]}/>
+                            value={[value1,value2]}
+                        />
+                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
                 </div>
             </div>
@@ -52,41 +58,3 @@ function HW11() {
 }
 
 export default HW11
-
-
-/*
-<span class="base-Slider-root">
-  <span class="base-Slider-rail"></span>
-  <span class="base-Slider-track"></span>
-  <span
-    data-index="0"
-    class="base-Slider-mark base-Slider-markActive"
-    style="left: 0%;"
-  ></span>
-  <span
-    aria-hidden="true"
-    data-index="0"
-    class="base-Slider-markLabel base-Slider-markLabelActive"
-    style="left: 0%;"
-    >0</span
-  >
-  <span data-index="1" class="base-Slider-mark" style="left: 50%;"></span>
-  <span
-    aria-hidden="true"
-    data-index="1"
-    class="base-Slider-markLabel"
-    style="left: 50%;"
-    >50</span
-  >
-  <span data-index="2" class="base-Slider-mark" style="left: 100%;"></span>
-  <span
-    aria-hidden="true"
-    data-index="2"
-    class="base-Slider-markLabel"
-    style="left: 100%;"
-    >100</span
-  >
-  <span class="base-Slider-thumb">
-    <input />
-  </span>
-</span> />*/
