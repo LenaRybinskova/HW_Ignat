23fef7c91e9ebe02d5a98a28e3798da5b007b8fd
«Elena <LenaRybinskova@gmail.com>
2024-08-23 15:28:46 +0300

    hw14 setSearchParams

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -34,22 +34,20 @@ const HW14 = () => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
+                if(res && res.data){
+                    setTechs(res.data.techs)
+                    setLoading(false)
+                }
             })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
+        setTimeout(sendQuery, 1500, value)
         // делает студент
-
         // добавить/заменить значение в квери урла
         // setSearchParams(
-
-        //
+        setSearchParams({value})
     }
 
     useEffect(() => {
